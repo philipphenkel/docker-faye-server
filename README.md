64489493aa944c7da5e965dc01e7822a592156da
Philipp Henkel <henkel42@gmail.com>
2016-10-11 23:37:48 +0200

    Add basic Faye server with logging option

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# docker-faye-server
+Basic Faye Server
+=================
+
+Faye is a publish-subscribe messaging system based on the Bayeux protocol. It is designed to allow client-side JavaScript programs to send messages to each other with low latency over HTTP.
+
+
+License
+-------
+
+Copyright (C) 2016 Philipp Henkel
+
+Licensed under the MIT License (MIT). See LICENSE file for more details.
---
+++ server.js
@@ -0,0 +1,44 @@
+var http = require('http');
+var faye = require('faye');
+
+
+// Configuration options
+var options = {
+    logging: process.env.FAYE_LOGGING || 'false',
+    listenPort: process.env.FAYE_PORT || 8000,
+    mount: process.env.FAYE_MOUNT ||  '/bayeux',
+    timeout: process.env.FAYE_TIMEOUT ||  45
+};
+
+console.log('Running Faye server\n' + JSON.stringify(options, null, 2));
+
+
+// Configure adapter
+var bayeux = new faye.NodeAdapter({mount: options.mount, timeout: options.timeout});
+
+
+// Log client connections + subscriptions
+if (options.logging === 'true') {
+
+    bayeux.on('handshake', function(clientId) {
+        console.log('[' + new Date() + '] Client ' + clientId + ' connected');
+    });
+
+    bayeux.on('disconnect', function(clientId) {
+        console.log('[' + new Date() + '] Client ' + clientId)+ ' disconnected';
+    });
+
+    bayeux.on('subscribe', function(clientId, channel) {
+        console.log('[' + new Date() + '] Client ' + clientId + ' subscribed to ' + channel);
+    });
+
+    bayeux.on('unsubscribe', function(clientId, channel) {
+        console.log('[' + new Date() + '] Client ' + clientId + ' unsubscribed from ' + channel);
+    });
+}
+
+
+// Set up server
+var httpServer = http.createServer();
+bayeux.attach(httpServer);
+httpServer.listen(options.listenPort);
